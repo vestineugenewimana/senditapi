e60a097e70fc6d548bcde8fed27588fa37d09f67
Igwaneza Bruce <36078186+helpybruce@users.noreply.github.com>
2018-11-06 11:43:05 +0200

    First commit--eslint&babel setup ready to go

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+/*  eslint linebreak-style:['error', 'windows'] */
+const app = express(); //  initialize express app
+app.use(bodyParser.urlencoded({ extended: true })); // allow submission of an object in the body
+app.use(bodyParser.json()); // transform data submitted into json
+app.use(morgan('dev')); // for logging the user data in color
+
+app.get('/', (req, res) => {
+  res.send({ message: 'sendit api is up and running' });
+});
+
+app.listen(process.env.PORT || 5000);
+
+export default app;

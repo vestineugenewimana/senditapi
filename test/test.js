d40f58db2f1da46c10dad2a678466c1206fbf3dc
knowbeeinc <help.pybruce@gmail.com>
2018-11-07 12:25:35 +0200

    test for a parcel with id 1, it passed

--- test/test.js
+++ test/test.js
@@ -5,11 +5,24 @@ import app from '../src/server';
 chai.use(chaiHttp);
 
 describe('PARCELS', () => {
-  describe('All parcels', () => {
+  describe('Get all parcels', () => {
     it('should return an object of all parcels', (done) => {
       chai
         .request(app)
         .get('/api/v1/parcels')
+        .end((err, res) => {
+          chai.expect(res.statusCode).to.be.equal(200);
+          chai.expect(res.body).to.be.a('object');
+          done();
+        });
+    });
+  });
+  describe('Get one parcel with id 1', () => {
+    it('should return one parcel object', (done) => {
+      const id = 1;
+      chai
+        .request(app)
+        .get(`/api/v1/parcels/${id}`)
         .end((err, res) => {
           chai.expect(res.statusCode).to.be.equal(200);
           chai.expect(res.body).to.be.a('object');

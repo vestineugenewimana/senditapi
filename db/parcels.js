7caff9957c1b930ca10832e5ae9522efd82d13f9
knowbeeinc <help.pybruce@gmail.com>
2018-11-07 14:39:10 +0200

    added user data dummy

--- db/parcels.js
+++ db/parcels.js
@@ -1,35 +1,35 @@
 const parcels = [
   {
     id: 1,
-    pickupLocation: "Kigali, KG Avenue 25 est",
-    destinationLocation: "Nyanza, Nyanza Rd",
-    weight: "15 g",
-    quantity: "5",
-    comment: "Laptop cables"
+    pickupLocation: 'Kigali, KG Avenue 25 est',
+    destinationLocation: 'Nyanza, Nyanza Rd',
+    weight: '15 g',
+    quantity: '5',
+    comment: 'Laptop cables',
   },
   {
     id: 2,
-    pickupLocation: "Kacyiru Library",
-    destinationLocation: "Muhanga district Library",
-    weight: "2 kg",
-    quantity: "20",
-    comment: "Books"
+    pickupLocation: 'Kacyiru Library',
+    destinationLocation: 'Muhanga district Library',
+    weight: '2 kg',
+    quantity: '20',
+    comment: 'Books',
   },
   {
     id: 3,
-    pickupLocation: "Nyagatare",
-    destinationLocation: "Huye, National Museum",
-    weight: "800 g",
-    quantity: "2",
-    comment: "Statues"
+    pickupLocation: 'Nyagatare',
+    destinationLocation: 'Huye, National Museum',
+    weight: '800 g',
+    quantity: '2',
+    comment: 'Statues',
   },
   {
     id: 4,
-    pickupLocation: "Kigali, KG Avenue 5",
-    destinationLocation: "Musanze Hospital",
-    weight: "150 g",
-    quantity: "1",
-    comment: "Laptop"
-  }
+    pickupLocation: 'Kigali, KG Avenue 5',
+    destinationLocation: 'Musanze Hospital',
+    weight: '150 g',
+    quantity: '1',
+    comment: 'Laptop',
+  },
 ];
-export default parcels
+export default parcels;
---
+++ db/users.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    id: '1',
+    names: 'test name',
+    location: 'Kigali, kg avenue 25 est',
+    email: 'test@gmail.com',
+    password: 'password',
+  },
+  {
+    id: '2',
+    names: 'user2 name',
+    location: 'Nyanza, avenue 58 street',
+    email: 'user2@gmail.com',
+    password: 'passcode',
+  },
+];
+export default users;

2cb7cbe5864ba26881a016d7c33d5af26bf84f2e
knowbeeinc <help.pybruce@gmail.com>
2018-11-09 02:39:48 +0200

    restructured the user db

--- db/users.js
+++ db/users.js
@@ -5,6 +5,26 @@ const users = [
     location: 'Kigali, kg avenue 25 est',
     email: 'test@gmail.com',
     password: 'password',
+    parcels: [
+      {
+        id: 1,
+        pickupLocation: 'Kigali, KG Avenue 25 est',
+        destinationLocation: 'Nyanza, Nyanza Rd',
+        weight: '15 g',
+        quantity: '5',
+        status: 'pending',
+        comment: 'Laptop cables',
+      },
+      {
+        id: 2,
+        pickupLocation: 'Nyagatare',
+        destinationLocation: 'Huye, National Museum',
+        weight: '800 g',
+        quantity: '2',
+        status: 'pending',
+        comment: 'Statues',
+      },
+    ],
   },
   {
     id: '2',
@@ -12,6 +32,17 @@ const users = [
     location: 'Nyanza, avenue 58 street',
     email: 'user2@gmail.com',
     password: 'passcode',
+    parcels: [
+      {
+        id: 1,
+        pickupLocation: 'Kacyiru Library',
+        destinationLocation: 'Muhanga district Library',
+        weight: '2 kg',
+        quantity: '20',
+        status: 'pending',
+        comment: 'Books',
+      },
+    ],
   },
 ];
 export default users;

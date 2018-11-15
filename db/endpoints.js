46a289c1733b04d5353b3afdcc3839dced3494ec
helpybruce <help.pybruce@gmail.com>
2018-11-16 00:12:51 +0200

    added a new endpoint on endpoints homepage

--- db/endpoints.js
+++ db/endpoints.js
@@ -1,20 +1,21 @@
 const endpoints = {
   Parcelendpoints: [
     {
-      allparcels: '/api/v1/parcels',
-      oneparcel: '/api/v1/parcels/:id',
-      createAparcel: '/api/v1/parcels',
-      cancelParcelOrder: '/api/v1/parcels/:id/cancel',
-      deleteAparcel: '/api/v1/parcels/:id',
-    },
+      allparcels: "/api/v1/parcels",
+      oneparcel: "/api/v1/parcels/:id",
+      createAparcel: "/api/v1/parcels",
+      ChangeParcelStatus: "/api/v1/parcels/status/:parcelId",
+      cancelParcelOrder: "/api/v1/parcels/:id/cancel",
+      deleteAparcel: "/api/v1/parcels/:id"
+    }
   ],
   Userendpoints: [
     {
-      allUsers: '/api/v1/users',
-      oneparcel: '/api/v1/users/register',
-      loginUser: '/api/v1/users/login',
-      userParcels: '/api/v1/users/:userId/parcels',
-    },
-  ],
+      allUsers: "/api/v1/users",
+      oneparcel: "/api/v1/users/register",
+      loginUser: "/api/v1/users/login",
+      userParcels: "/api/v1/users/:userId/parcels"
+    }
+  ]
 };
 export default endpoints;
